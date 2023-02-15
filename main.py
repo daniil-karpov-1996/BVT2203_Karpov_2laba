05cfd4d61ccbda4046202fcbcfdc515320b4802a
daniil-karpov-1996 <daniil.karpov.1996@mail.ru>
2023-02-15 22:42:57 +0300

    final

--- main.py
+++ main.py
@@ -5,8 +5,15 @@ appid = '29d09b29d42f90dfe468c166e44d0fd1'
 res = requests.get("http://api.openweathermap.org/data/2.5/weather",
                    params={'q': city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
 data = res.json()
-print("Город:", city)
-print("Погодные условия:", data['weather'][0]['description'])
-print("Температура:", data['main']['temp'])
-print("Минимальная температура:", data['main']['temp_min'])
-print("Максимальная температура", data['main']['temp_max'])
+print('Скорость ветра и видимость на сегодня:')
+print('Видимость:      ', data['visibility'])
+print('Скорость ветра: ', data['wind']['speed'], 'м/c')
+res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
+                   params={'q': city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+data = res.json()
+print('\n')
+print("Прогноз погоды на неделю:")
+for i in data['list']:
+    print('Дата', i['dt_txt'].split()[0], 'Время', i['dt_txt'].split()[1])
+    print('Видимость:      ', i['visibility'])
+    print('Скорость ветра: ', i['wind']['speed'], 'м/c', '\n')
