b56c11b8fe47d8c2a8bd6849125bee600487c1fe
daniil-karpov-1996 <daniil.karpov.1996@mail.ru>
2023-02-15 22:28:32 +0300

    testing

--- main.py
+++ main.py
@@ -5,4 +5,8 @@ appid = '29d09b29d42f90dfe468c166e44d0fd1'
 res = requests.get("http://api.openweathermap.org/data/2.5/weather",
                    params={'q': city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
 data = res.json()
-print(data)
+print("Город:", city)
+print("Погодные условия:", data['weather'][0]['description'])
+print("Температура:", data['main']['temp'])
+print("Минимальная температура:", data['main']['temp_min'])
+print("Максимальная температура", data['main']['temp_max'])
